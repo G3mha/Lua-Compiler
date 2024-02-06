810e577f82c05f1ac11ae09ba41d7be5b921c848
Enricco Gemha <riccogemha@gmail.com>
2024-02-06 10:38:42 -0300

    Change to OOP

--- main.swift
+++ main.swift
@@ -1,29 +1,28 @@
 import Foundation
 
-func calculator(input: String) -> Int {
-  // create a variable to store the final result
-  var result = 0
-  
-  // first split by "+"
-  let splitBySum = input.components(separatedBy: "+")
-  
-  // than split each element by "-"
-  for sum in splitBySum {
-    let sub = sum.components(separatedBy: "-")
-    if sub.count > 1 {
-      let first = Int(sub[0]) ?? 0
-      let second = Int(sub[1]) ?? 0
-      // if there is a "-" in the element, it's a sub element
-      result += first - second
-    } else {
-      // if there is no "-" in the element, it's a sum element
-      result += Int(sum) ?? 0
-    }
+class Compiler {
+  static func run() {
+    let inputString = CommandLine.arguments[1]
+    let result = sumSub(input: inputString)
+    print(result)
   }
-
-  return result
+  
+  static func sumSub(input: String) -> Int {
+    var result = 0
+    let splitBySum = input.components(separatedBy: "+")
+    for sum in splitBySum {
+      let sub = sum.components(separatedBy: "-")
+      if sub.count > 1 {
+        let first = Int(sub[0]) ?? 0
+        let second = Int(sub[1]) ?? 0
+        result += first - second
+      } else {
+        result += Int(sum) ?? 0
+      }
+    }
+    return result
+  }
 }
 
-let inputString = CommandLine.arguments[1]
-let result = calculator(input: inputString)
-print(result)
+// Run the compiler
+Compiler.run()
